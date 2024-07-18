17156a9afb66990546a9fd0c4592c50fe25997f0
caiolaytynher <csrlaytynher@proton.me>
2024-07-18 18:18:29 -0300

    a bunch of changes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -90,6 +90,10 @@ P.S. You can delete this when you're done too. It's your config now! :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- For nvim-tree
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 -- [[ Setting options ]]
 -- See `:help vim.opt`
 -- NOTE: You can change these options as you wish!
@@ -148,6 +152,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Disable wrap
+vim.opt.wrap = false
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
@@ -184,6 +191,13 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Opens a new terminal in split horizontal view with fish shell
+vim.keymap.set('n', '<leader>t', function()
+  vim.cmd 'horizontal terminal fish'
+end, { desc = 'Open [T]erminal' })
+
+vim.keymap.set('n', '<leader>v', ':vsplit<CR>', { desc = 'Move focus to the upper window' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
@@ -195,6 +209,97 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
     vim.highlight.on_yank()
+  end,
+})
+
+vim.api.nvim_create_autocmd('BufEnter', {
+  desc = 'Set .h files to be C files instead of C++ files.',
+  pattern = { '*.h' },
+  group = vim.api.nvim_create_augroup('caio-header', { clear = true }),
+  callback = function()
+    vim.opt.filetype = 'c'
+  end,
+})
+
+vim.api.nvim_create_autocmd('BufEnter', {
+  desc = 'Set Python files to wrap with gq only 72 lines.',
+  pattern = { '*.py' },
+  group = vim.api.nvim_create_augroup('caio-python-wrap', { clear = true }),
+  callback = function()
+    vim.opt.textwidth = 72
+    vim.opt.formatoptions = 'jcroql'
+
+    vim.keymap.set('n', '<leader>rf', function()
+      local buf = vim.api.nvim_get_current_buf()
+      local file = vim.api.nvim_buf_get_name(buf)
+
+      vim.cmd('horizontal terminal python ' .. file)
+    end, { desc = '[R]un [F]ile}' })
+
+    vim.keymap.set('n', '<leader>rm', function()
+      local buf = vim.api.nvim_get_current_buf()
+      local file = vim.api.nvim_buf_get_name(buf)
+      local cwd = vim.fn.getcwd()
+
+      -- Remove escape characters that make gsub stop working
+      file = string.gsub(file, '%-', '')
+      cwd = string.gsub(cwd, '%-', '')
+
+      -- Adding the last slash that cwd does not provide
+      file = string.gsub(file, cwd .. '/', '')
+      file = string.gsub(file, '.py', '')
+      file = string.gsub(file, '/', '.')
+
+      vim.cmd('horizontal terminal python -m' .. file)
+    end, { desc = '[R]un [M]odule}' })
+
+    vim.keymap.set('n', '<leader>rd', function()
+      local ok, dap = pcall(require, 'dap')
+      if not ok then
+        print 'DAP not found'
+        return
+      end
+
+      dap.continue()
+    end, { desc = '[R]un [D]ebugger}' })
+  end,
+})
+
+vim.api.nvim_create_autocmd('VimEnter', {
+  desc = 'Provide support for make if the project has a Makefile.',
+  group = vim.api.nvim_create_augroup('caio-make', { clear = true }),
+  callback = function()
+    -- If the Makefile doesn't exist, do nothing
+    local makefile = io.open 'Makefile'
+    if makefile then
+      io.close(makefile)
+    else
+      return
+    end
+
+    vim.keymap.set('n', '<leader>mb', function()
+      vim.cmd 'horizontal terminal make'
+    end, { desc = 'Run [M]ake [B]uild' })
+
+    vim.keymap.set('n', '<leader>mr', function()
+      vim.cmd 'horizontal terminal make run'
+    end, { desc = 'Run [M]ake [R]un' })
+
+    vim.keymap.set('n', '<leader>md', function()
+      local ok, dap = pcall(require, 'dap')
+      if not ok then
+        print 'DAP not found'
+        return
+      end
+
+      local obj = vim.system({ 'make' }, { text = true }):wait()
+      if obj.code ~= 0 then
+        vim.api.nvim_echo({ { obj.stderr, 'Error' } }, false, {})
+        return
+      end
+
+      dap.continue()
+    end, { desc = 'Run [M]ake then start the [D]ebugger' })
   end,
 })
 
@@ -462,7 +567,7 @@ require('lazy').setup {
           map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
 
           -- Jump to the implementation of the word under your cursor.
-          --  Useful when your language has ways of declaring types without an actual implementation.
+          --  Useful when your language has ways of declaring typos without an actual implementation.
           map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
 
           -- Jump to the type of the word under your cursor.
@@ -531,7 +636,14 @@ require('lazy').setup {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {
+          -- cmd = { "clangd", unpack(require("custom.clangd").flags) },
+          -- TODO: Could include cmd, but not sure those were all relevant flags.
+          --    looks like something i would have added while i was floundering
+          init_options = { clangdFileStatus = true },
+
+          filetypes = { 'c', 'cpp' },
+        },
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
@@ -620,7 +732,7 @@ require('lazy').setup {
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        python = { 'isort', 'black' },
+        python = { 'ruff', 'ruff_format' },
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
@@ -726,6 +838,29 @@ require('lazy').setup {
     end,
   },
 
+  { -- Linting
+    'mfussenegger/nvim-lint',
+    event = { 'BufReadPre', 'BufNewFile' },
+    -- opts = {
+    --   linters_by_ft = {
+    --     python = { 'mypy' },
+    --   },
+    -- },
+    config = function()
+      local lint = require 'lint'
+      lint.linters_by_ft = { python = { 'ruff' } }
+
+      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+        group = vim.api.nvim_create_augroup('caio-lint', { clear = true }),
+        callback = function()
+          -- try_lint without arguments runs the linters defined in `linters_by_ft`
+          -- for the current filetype
+          lint.try_lint()
+        end,
+      })
+    end,
+  },
+
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is
@@ -738,7 +873,7 @@ require('lazy').setup {
       local onedark = require 'onedark'
       onedark.setup {
         -- Set a style preset. 'dark' is default.
-        style = 'warmer', -- dark, darker, cool, deep, warm, warmer, light
+        style = 'dark', -- dark, darker, cool, deep, warm, warmer, light
       }
       onedark.load()
 
@@ -798,7 +933,7 @@ require('lazy').setup {
 
       ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup {
-        ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
+        ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'python' },
         -- Autoinstall languages that are not installed
         auto_install = true,
         highlight = { enable = true },
@@ -811,6 +946,215 @@ require('lazy').setup {
       --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
       --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+    end,
+  },
+
+  { -- Debugger
+    'mfussenegger/nvim-dap',
+    dependencies = {
+      'rcarriga/nvim-dap-ui',
+      'theHamsta/nvim-dap-virtual-text',
+      'nvim-neotest/nvim-nio',
+      'williamboman/mason.nvim',
+    },
+    config = function()
+      local dap = require 'dap'
+      local ui = require 'dapui'
+
+      require('dapui').setup()
+      require('nvim-dap-virtual-text').setup {}
+
+      dap.adapters.cppdbg = {
+        id = 'cppdbg',
+        type = 'executable',
+        command = vim.fn.stdpath 'data' .. '/mason/bin/OpenDebugAD7',
+      }
+      dap.configurations.c = {
+        {
+          name = 'Launch file',
+          type = 'cppdbg',
+          request = 'launch',
+          program = '${workspaceFolder}/bin/${workspaceFolderBasename}',
+          cwd = '${workspaceFolder}',
+          stopAtEntry = true,
+        },
+      }
+
+      dap.adapters.python = function(cb, config)
+        if config.request == 'attach' then
+          ---@diagnostic disable-next-line: undefined-field
+          local port = (config.connect or config).port
+          ---@diagnostic disable-next-line: undefined-field
+          local host = (config.connect or config).host or '127.0.0.1'
+          cb {
+            type = 'server',
+            port = assert(port, '`connect.port` is required for a python `attach` configuration'),
+            host = host,
+            options = {
+              source_filetype = 'python',
+            },
+          }
+        else
+          cb {
+            type = 'executable',
+            command = vim.fn.stdpath 'data' .. '/mason/bin/debugpy-adapter',
+            options = {
+              source_filetype = 'python',
+            },
+          }
+        end
+      end
+      dap.configurations.python = {
+        {
+          -- The first three options are required by nvim-dap
+          type = 'python', -- the type here established the link to the adapter definition: `dap.adapters.python`
+          request = 'launch',
+          name = 'Launch file',
+
+          -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+
+          program = '${file}', -- This configuration will launch the current file if used.
+          pythonPath = function()
+            -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+            -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+            -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+            local venv = os.getenv 'VIRTUAL_ENV'
+            if venv then
+              return venv .. '/bin/python'
+            else
+              return '/usr/bin/python'
+            end
+          end,
+        },
+      }
+
+      vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Toggle [B]reakpoint' })
+      vim.keymap.set('n', '<leader>gb', dap.run_to_cursor)
+
+      -- Eval var under cursor
+      vim.keymap.set('n', '<leader>?', function()
+        require('dapui').eval(nil, { enter = true })
+      end)
+
+      vim.keymap.set('n', '<F5>', dap.continue)
+      vim.keymap.set('n', '<F10>', dap.step_into)
+      vim.keymap.set('n', '<F11>', dap.step_over)
+      vim.keymap.set('n', '<F12>', dap.step_out)
+      vim.keymap.set('n', '<F1>', dap.step_back)
+      vim.keymap.set('n', '<F2>', dap.restart)
+      vim.keymap.set('n', '<F3>', dap.close)
+      vim.keymap.set('n', '<F4>', dap.disconnect)
+      vim.keymap.set('n', '<leader>u', ui.toggle, { desc = 'Toggle debugger [U]I' })
+
+      dap.listeners.before.attach.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        ui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        ui.close()
+      end
+    end,
+  },
+  { -- File tree
+    'nvim-tree/nvim-tree.lua',
+    version = '*',
+    lazy = false,
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      require('nvim-tree').setup {
+        sort = {
+          sorter = 'case_sensitive',
+        },
+        view = {
+          width = 30,
+        },
+        renderer = {
+          group_empty = true,
+        },
+        filters = {
+          dotfiles = false,
+        },
+
+        on_attach = function(bufnr)
+          local api = require 'nvim-tree.api'
+
+          local function opts(desc)
+            return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+          end
+
+          -- default mappings
+          -- api.config.mappings.default_on_attach(bufnr)
+
+          -- custom mappings
+
+          vim.keymap.set('n', '<leader>f', api.tree.toggle, { desc = 'Toggle file tree' })
+          -- vim.keymap.set('n', '<C-]>', api.tree.change_root_to_node, opts 'CD')
+          -- vim.keymap.set('n', '<C-e>', api.node.open.replace_tree_buffer, opts 'Open: In Place')
+          -- vim.keymap.set('n', '<C-k>', api.node.show_info_popup, opts 'Info')
+          -- vim.keymap.set('n', '<C-r>', api.fs.rename_sub, opts 'Rename: Omit Filename')
+          -- vim.keymap.set('n', '<C-t>', api.node.open.tab, opts 'Open: New Tab')
+          vim.keymap.set('n', 'v', api.node.open.vertical, opts 'Open: Vertical Split')
+          -- vim.keymap.set('n', '<C-x>', api.node.open.horizontal, opts 'Open: Horizontal Split')
+          vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts 'Close Directory')
+          vim.keymap.set('n', 'l', api.node.open.edit, opts 'Open')
+          -- vim.keymap.set('n', '<Tab>', api.node.open.preview, opts 'Open Preview')
+          -- vim.keymap.set('n', '>', api.node.navigate.sibling.next, opts 'Next Sibling')
+          -- vim.keymap.set('n', '<', api.node.navigate.sibling.prev, opts 'Previous Sibling')
+          -- vim.keymap.set('n', '.', api.node.run.cmd, opts 'Run Command')
+          -- vim.keymap.set('n', '-', api.tree.change_root_to_parent, opts 'Up')
+          vim.keymap.set('n', 'n', api.fs.create, opts 'Create File Or Directory')
+          vim.keymap.set('n', 'bd', api.marks.bulk.delete, opts 'Delete Bookmarked')
+          -- vim.keymap.set('n', 'bt', api.marks.bulk.trash, opts 'Trash Bookmarked')
+          vim.keymap.set('n', '<C-m>', api.marks.bulk.move, opts 'Move Bookmarked')
+          -- vim.keymap.set('n', 'B', api.tree.toggle_no_buffer_filter, opts 'Toggle Filter: No Buffer')
+          vim.keymap.set('n', 'c', api.fs.copy.node, opts 'Copy')
+          -- vim.keymap.set('n', 'C', api.tree.toggle_git_clean_filter, opts 'Toggle Filter: Git Clean')
+          -- vim.keymap.set('n', '[c', api.node.navigate.git.prev, opts 'Prev Git')
+          -- vim.keymap.set('n', ']c', api.node.navigate.git.next, opts 'Next Git')
+          vim.keymap.set('n', 'd', api.fs.remove, opts 'Delete')
+          -- vim.keymap.set('n', 'D', api.fs.trash, opts 'Trash')
+          -- vim.keymap.set('n', 'E', api.tree.expand_all, opts 'Expand All')
+          -- vim.keymap.set('n', 'e', api.fs.rename_basename, opts 'Rename: Basename')
+          -- vim.keymap.set('n', ']e', api.node.navigate.diagnostics.next, opts 'Next Diagnostic')
+          -- vim.keymap.set('n', '[e', api.node.navigate.diagnostics.prev, opts 'Prev Diagnostic')
+          -- vim.keymap.set('n', 'F', api.live_filter.clear, opts 'Live Filter: Clear')
+          -- vim.keymap.set('n', 'f', api.live_filter.start, opts 'Live Filter: Start')
+          -- vim.keymap.set('n', 'g?', api.tree.toggle_help, opts 'Help')
+          -- vim.keymap.set('n', 'gy', api.fs.copy.absolute_path, opts 'Copy Absolute Path')
+          -- vim.keymap.set('n', 'ge', api.fs.copy.basename, opts 'Copy Basename')
+          -- vim.keymap.set('n', 'H', api.tree.toggle_hidden_filter, opts 'Toggle Filter: Dotfiles')
+          -- vim.keymap.set('n', 'I', api.tree.toggle_gitignore_filter, opts 'Toggle Filter: Git Ignore')
+          -- vim.keymap.set('n', 'J', api.node.navigate.sibling.last, opts 'Last Sibling')
+          -- vim.keymap.set('n', 'K', api.node.navigate.sibling.first, opts 'First Sibling')
+          -- vim.keymap.set('n', 'L', api.node.open.toggle_group_empty, opts 'Toggle Group Empty')
+          -- vim.keymap.set('n', 'M', api.tree.toggle_no_bookmark_filter, opts 'Toggle Filter: No Bookmark')
+          vim.keymap.set('n', 'm', api.marks.toggle, opts 'Toggle Bookmark')
+          -- vim.keymap.set('n', 'o', api.node.open.edit, opts 'Open')
+          -- vim.keymap.set('n', 'O', api.node.open.no_window_picker, opts 'Open: No Window Picker')
+          -- vim.keymap.set('n', 'p', api.fs.paste, opts 'Paste')
+          -- vim.keymap.set('n', 'P', api.node.navigate.parent, opts 'Parent Directory')
+          -- vim.keymap.set('n', 'q', api.tree.close, opts 'Close')
+          vim.keymap.set('n', 'r', api.fs.rename, opts 'Rename')
+          vim.keymap.set('n', 'R', api.tree.reload, opts 'Refresh')
+          -- vim.keymap.set('n', 's', api.node.run.system, opts 'Run System')
+          vim.keymap.set('n', 'S', api.tree.search_node, opts 'Search')
+          -- vim.keymap.set('n', 'u', api.fs.rename_full, opts 'Rename: Full Path')
+          -- vim.keymap.set('n', 'U', api.tree.toggle_custom_filter, opts 'Toggle Filter: Hidden')
+          -- vim.keymap.set('n', 'W', api.tree.collapse_all, opts 'Collapse')
+          vim.keymap.set('n', 'x', api.fs.cut, opts 'Cut')
+          vim.keymap.set('n', 'y', api.fs.copy.filename, opts 'Copy Name')
+          vim.keymap.set('n', 'Y', api.fs.copy.relative_path, opts 'Copy Relative Path')
+          vim.keymap.set('n', '<2-LeftMouse>', api.node.open.edit, opts 'Open')
+          vim.keymap.set('n', '<2-RightMouse>', api.tree.change_root_to_node, opts 'CD')
+        end,
+      }
     end,
   },
 
@@ -837,7 +1181,8 @@ require('lazy').setup {
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
 
--- Clipboard was not working in wayland, so I tryied this code that I found at https://github.com/neovim/neovim/issues/25466 and it works
+-- Clipboard was not working in wayland, so I tryied this code that I found at
+-- https://github.com/neovim/neovim/issues/25466 and it works
 if vim.env.WAYLAND_DISPLAY and vim.fn.executable 'wl-copy' and vim.fn.executable 'wl-paste' then
   vim.g.clipboard = {
     name = 'wl-copy',
